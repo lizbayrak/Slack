5818f8923042e7cb10c1bf9d381d42fe921211e6
lizbayrak <62037175+lizbayrak@users.noreply.github.com>
2020-05-13 15:59:04 +0100

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,12 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         //Channel
+        
+   String s= "a"; //changed from GitHub
+    if(s.contains("a")){
+        System.out.println("contains");
+    }else{
+        System.out.println("not");
+}
     }
 }
