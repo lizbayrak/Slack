db7fc04e607bdfabbc3ad8d240731c9a379cc6a9
lizbayrak <liz.bayrak@yahoo.co.uk>
2020-05-13 15:57:36 +0100

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,11 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         //Channel
+
+        int x=0; //changed from IntelliJ
+        if (x==0){
+            System.out.println("equal");
+        }
+
     }
 }
